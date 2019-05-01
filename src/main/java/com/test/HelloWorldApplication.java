c5ceed150c2689ee8148dd1a83748dfbd2342aea
rajkumar katanor <rkatanor@gmail.com>
2019-05-01 12:28:40 +0530

    making it as web application

--- src/main/java/com/test/HelloWorldApplication.java
+++ src/main/java/com/test/HelloWorldApplication.java
@@ -2,12 +2,14 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController(value="/rs")
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
@@ -15,6 +17,11 @@ public class HelloWorldApplication {
 	@GetMapping(value="/hello")
 	public String helloWorld() {
 		return "Hello world welcome to AWS";
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(HelloWorldApplication.class);
 	}
 
 }
